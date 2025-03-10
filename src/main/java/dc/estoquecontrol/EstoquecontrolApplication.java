510f5d0c63eeef8f48cde7c2037dea2c75178154
Lucas Miranda <lucasvnx6@gmail.com>
2025-03-10 18:53:52 -0300

    First commit

---
+++ src/main/java/dc/estoquecontrol/EstoquecontrolApplication.java
@@ -0,0 +1,13 @@
+package dc.estoquecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquecontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dc/estoquecontrol/EstoquecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package dc.estoquecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
