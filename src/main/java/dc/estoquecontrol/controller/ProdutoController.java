fc1b302e2d4e5a4b3c1148597752b51083a2e8cc
Lucas Miranda <lucasvnx6@gmail.com>
2025-03-10 19:41:57 -0300

    Classe ProdutoController criada

---
+++ src/main/java/dc/estoquecontrol/controller/ProdutoController.java
@@ -0,0 +1,14 @@
+package dc.estoquecontrol.controller;
+
+import dc.estoquecontrol.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+}
