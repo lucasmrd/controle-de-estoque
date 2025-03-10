043fe7bf2d9e92602ee5253ad19ea745db002490
Lucas Miranda <lucasvnx6@gmail.com>
2025-03-10 19:41:17 -0300

    Repositorio de Produto criado

---
+++ src/main/java/dc/estoquecontrol/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package dc.estoquecontrol.repository;
+
+import dc.estoquecontrol.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+}
