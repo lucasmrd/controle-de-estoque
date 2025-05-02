fcb8ca015812d8f082d05632ecf6d8a90e586e1f
Lucas Miranda <lucasvnx6@gmail.com>
2025-05-02 14:18:57 -0300

    SecurityConfigurations refatorado

--- src/main/java/dc/estoquecontrol/infra/security/SecurityConfigurations.java
+++ src/main/java/dc/estoquecontrol/infra/security/SecurityConfigurations.java
@@ -1,14 +1,10 @@
-package dc.estoquecontrol.infra;
+package dc.estoquecontrol.infra.security;
 
-import jakarta.servlet.SessionCookieConfig;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,60 +18,23 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> {})
+                //.cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/logout").permitAll()
-                        .requestMatchers("/api/me").authenticated()// permite login e logout
-                        .anyRequest().authenticated() // exige autenticação para o resto
-                )
-                .formLogin(form -> form
-                        .loginProcessingUrl("/login") // endpoint que recebe POST com username/password
-                        .successHandler((req, res, auth) -> res.setStatus(HttpServletResponse.SC_OK))
-                        .failureHandler((req, res, exc) -> res.setStatus(HttpServletResponse.SC_UNAUTHORIZED))
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessHandler((req, res, auth) -> {
-                                    System.out.println("Sessão atual: " + req.getSession(false));
-                                    res.setStatus(HttpServletResponse.SC_OK);
-                                })
-                        //.logoutSuccessHandler((req, res, auth) -> res.setStatus(HttpServletResponse.SC_OK))
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.NEVER)
-                )
-                .build();
-        /*
-        return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
-                    //req.requestMatchers(HttpMethod.POST, "/api/login").permitAll();
-                    //req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
-                    req.anyRequest().permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/api/login").permitAll();
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
+                    req.anyRequest().authenticated();
                 })
-                //.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-
-         */
     }
-
-//    @Bean
-//    public ServletContextInitializer servletContextInitializer() {
-//        return servletContext -> {
-//            SessionCookieConfig sessionCookieConfig = servletContext.getSessionCookieConfig();
-//            sessionCookieConfig.setMaxAge(60 * 60 * 24 * 7); // 7 dias
-//            sessionCookieConfig.setHttpOnly(true);
-//            sessionCookieConfig.setPath("/");
-//            // sessionCookieConfig.setSecure(true); // SOMENTE EM PRODUCAO
-//            //sessionCookieConfig.setDomain("seudominio.com"); // opcional, se necessário
-//        };
-//    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
@@ -86,5 +45,4 @@ public class SecurityConfigurations {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
