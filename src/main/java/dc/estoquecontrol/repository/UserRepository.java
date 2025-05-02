ff8eab5ed552da7dfd2055a64983328e75cc59ce
Lucas Miranda <lucasvnx6@gmail.com>
2025-05-02 14:18:09 -0300

    Entidade User e seu repositorio criados

---
+++ src/main/java/dc/estoquecontrol/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dc.estoquecontrol.repository;
+
+import dc.estoquecontrol.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByUsername(String username);
+}
