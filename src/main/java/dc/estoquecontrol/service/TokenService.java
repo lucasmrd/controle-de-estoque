303f174d5e80a385d5b9f4ce0b6eea11676792ac
Lucas Miranda <lucasvnx6@gmail.com>
2025-05-02 14:18:18 -0300

    TokenService criado

---
+++ src/main/java/dc/estoquecontrol/service/TokenService.java
@@ -0,0 +1,51 @@
+package dc.estoquecontrol.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import dc.estoquecontrol.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${JWT_SECRET:123}")
+    private String secret;
+
+    public String gerarToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("cantina")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("cantina")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusMonths(1).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
