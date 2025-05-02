5f9df57fb978ee5637a22dac589e5883b7207edc
Lucas Miranda <lucasvnx6@gmail.com>
2025-05-02 14:18:31 -0300

    AutenticacaoService criado

---
+++ src/main/java/dc/estoquecontrol/service/AutenticacaoService.java
@@ -0,0 +1,25 @@
+package dc.estoquecontrol.service;
+
+import dc.estoquecontrol.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado: " + username);
+        }
+        return user;
+    }
+}
