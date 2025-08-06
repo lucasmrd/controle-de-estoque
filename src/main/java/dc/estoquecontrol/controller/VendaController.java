29011d789f79ec2cff1b3bac90f5e5727123fe94
Lucas Miranda <lucasvnx6@gmail.com>
2025-08-06 18:24:03 -0300

    Atualizando controller

--- src/main/java/dc/estoquecontrol/controller/VendaController.java
+++ src/main/java/dc/estoquecontrol/controller/VendaController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/vendas")
@@ -38,7 +41,6 @@ public class VendaController {
         return service.filtrarPorMesEAno(mes, ano, pageable);
     }
 
-    // Filtragem por Mês
     @GetMapping("/filtrar/mes")
     public ResponseEntity<Page<MostrarVendaResponse>> filtrarPorMes(
             @RequestParam Integer mes,
@@ -47,12 +49,38 @@ public class VendaController {
         return service.filtrarPorMes(mes, pageable);
     }
 
-    // Filtragem por Ano
     @GetMapping("/filtrar/ano")
     public ResponseEntity<Page<MostrarVendaResponse>> filtrarPorAno(
             @RequestParam Integer ano,
             @PageableDefault Pageable pageable
     ) {
         return service.filtrarPorAno(ano, pageable);
+    }
+
+    @GetMapping("/filtrar/nome")
+    public ResponseEntity<Page<MostrarVendaResponse>> filtrarPorNome(
+            @RequestParam String nome,
+            @PageableDefault Pageable pageable
+    ) {
+        return service.filtrarPorNome(nome, pageable);
+    }
+
+    @GetMapping("/filtrar/periodo")
+    public ResponseEntity<Page<MostrarVendaResponse>> filtrarPorPeriodo(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicio,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim,
+            @PageableDefault Pageable pageable
+    ) {
+        return service.filtrarPorPeriodo(dataInicio, dataFim, pageable);
+    }
+
+    @GetMapping("/filtrar/periodo-nome")
+    public ResponseEntity<Page<MostrarVendaResponse>> filtrarPorPeriodoENome(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicio,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim,
+            @RequestParam String nome,
+            @PageableDefault Pageable pageable
+    ) {
+        return service.filtrarPorPeriodoENome(dataInicio, dataFim, nome, pageable);
     }
 }
